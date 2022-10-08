909c01a992697622b0b3babc5279e7ed688bd9b9
Dierale <89657232+Dierale@users.noreply.github.com>
2022-10-07 23:29:12 -0500

    Added MainActivity Fragments
    Added fragments for the 3 main pages we're going to display in our testing with the MainActivity.  We have a fragment for the user's dashboard, the sessions/conference page, and where we'll put the menu for the 'more' section.

---
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/DashboardFragment.java
@@ -0,0 +1,47 @@
+package nwmsu.android.nursingsymposium;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DashboardFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DashboardFragment extends Fragment {
+
+    public DashboardFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment DashboardFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DashboardFragment newInstance() {
+        DashboardFragment fragment = new DashboardFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+}
