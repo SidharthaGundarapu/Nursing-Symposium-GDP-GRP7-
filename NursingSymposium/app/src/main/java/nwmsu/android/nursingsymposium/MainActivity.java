c404cd89b6309c359d1eb7ac4ad0fcbeaac93e67
Dierale <89657232+Dierale@users.noreply.github.com>
2022-10-07 23:37:54 -0500

    Update MainActivity.java
    Added logic to create bottom nav bar.

--- NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/MainActivity.java
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/MainActivity.java
@@ -1,14 +1,59 @@
 package nwmsu.android.nursingsymposium;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+
+    DashboardFragment dashboardFragment = new DashboardFragment();
+    SessionsFragment sessionsFragment = new SessionsFragment();
+    MoreFragment moreFragment = new MoreFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createBottomNav();
     }
+
+    private void createBottomNav() {
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        // Having some issues here I think. Had to do the casting to get it to work
+        // Pretty sure I'm just referring to this inappropriately.
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                (BottomNavigationView.OnNavigationItemSelectedListener) this);
+        bottomNavigationView.setSelectedItemId(R.id.userNav_sessions);
+    }
+
+    // Following a tutorial for this one. Not sure I need this
+    // Android does their navbar gen differently with the templated bottomNav stuff
+    // I need to look into this.
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.userNav_dashboard:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, dashboardFragment).commit();
+                return true;
+
+            case R.id.userNav_sessions:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, sessionsFragment).commit();
+                return true;
+
+            case R.id.userNav_more:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, moreFragment).commit();
+                return true;
+        }
+        return false;
+    }
+
 }
