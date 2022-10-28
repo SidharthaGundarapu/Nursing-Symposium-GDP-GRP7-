ff060e0b23cfe5a99d662537e04f6e4921b7caa4
AKV Phaneendra babu <indraakv@gmail.com>
2022-10-28 17:40:30 -0500

    Create LoginActivity.java

---
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/LoginActivity.java
@@ -0,0 +1,97 @@
+package com.example.nursingsymposium;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button login_btn;
+    TextInputEditText id_username;
+    TextInputEditText id_userpassword;
+    TextView forgotPassword, register;
+    ProgressDialog progressDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login_btn = findViewById(R.id.login_btn);
+        id_userpassword = findViewById(R.id.id_userpassword);
+        forgotPassword = findViewById(R.id.forgotPassword);
+        register = findViewById(R.id.register);
+        id_username = findViewById(R.id.id_username);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = id_username.getText().toString();
+                String password = id_userpassword.getText().toString();
+
+
+                if (TextUtils.isEmpty(username)) {
+                    Toast.makeText(LoginActivity.this, "Enter UserName", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    try {
+                        progressDialog = new ProgressDialog(LoginActivity.this);
+                        progressDialog.setMessage("Loading....");
+                        progressDialog.show();
+                        FirebaseAuth.getInstance().signInWithEmailAndPassword(username, password)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        progressDialog.cancel();
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(LoginActivity.this, "Login Succesfull", Toast.LENGTH_SHORT).show();
+                                            Intent intent = new Intent(LoginActivity.this, DashBoardActivity.class);
+                                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                            startActivity(intent);
+                                            finish();
+
+                                        } else {
+                                            Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+
+
+                    } catch (Exception e
+                    ) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
