cdec06344ec941f2f2dbd41297cdc0a29f9ebb10
Dierale <89657232+Dierale@users.noreply.github.com>
2022-10-28 22:42:55 -0500

    Added Activity Swap Buttons
    Added temporary buttons to swap between activities for testing purposes.

--- ConferenceApp/app/src/main/java/nwmsu/android/conferenceapp/ui/dashboard/DashboardFragment.java
+++ ConferenceApp/app/src/main/java/nwmsu/android/conferenceapp/ui/dashboard/DashboardFragment.java
@@ -1,15 +1,20 @@
 package nwmsu.android.conferenceapp.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import nwmsu.android.conferenceapp.MainActivity;
+import nwmsu.android.conferenceapp.R;
+import nwmsu.android.conferenceapp.StaffActivity;
 import nwmsu.android.conferenceapp.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
@@ -26,6 +31,16 @@ public class DashboardFragment extends Fragment {
 
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        Button staffSwapBTN = (Button) getView().findViewById( R.id.staffSwapBTN);
+        staffSwapBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Swap to the user actvitiy
+                Intent swapIntent = new Intent(getActivity(), StaffActivity.class);
+                startActivity( swapIntent);
+            }
+        });
         return root;
     }
 
