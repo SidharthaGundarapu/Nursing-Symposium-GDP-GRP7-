ae29e908c7ad6a04d12301ecc5b976bc5d7fed73
Dierale <89657232+Dierale@users.noreply.github.com>
2022-11-04 22:59:18 -0500

    Updated StaffActivity related Bindings
    Updated the staff menu and statistics fragments to use bindings in order to generate some proof of concept text on said fragments.  The approach now matches the other fragments in the app.

--- ConferenceApp/app/src/main/java/nwmsu/android/conferenceapp/ui/statistics/StatisticsFragment.java
+++ ConferenceApp/app/src/main/java/nwmsu/android/conferenceapp/ui/statistics/StatisticsFragment.java
@@ -3,14 +3,18 @@ package nwmsu.android.conferenceapp.ui.statistics;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import nwmsu.android.conferenceapp.R;
+import nwmsu.android.conferenceapp.databinding.FragmentStatisticsBinding;
 
 public class StatisticsFragment extends Fragment {
+
+    private FragmentStatisticsBinding binding;
 
     public StatisticsFragment() {
         // Required empty public constructor
@@ -32,7 +36,20 @@ public class StatisticsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
+        StatisticsViewModel statisticsViewModel =
+                new ViewModelProvider(this).get(StatisticsViewModel.class);
+
+        binding = FragmentStatisticsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textStatistics;
+        statisticsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
