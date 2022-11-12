93e7a8d211786d1fca0177ae5c73411d09267dab
AKV Phaneendra babu <indraakv@gmail.com>
2022-11-11 20:38:16 -0600

    Dashboard is changed

--- NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/DashBoardActivity.java
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/DashBoardActivity.java
@@ -1,4 +1,14 @@
+package com.example.nursingsymposium;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -18,20 +28,11 @@ public class DashBoardActivity extends AppCompatActivity {
         logout=findViewById(R.id.logout);
         id_dashboard_recycler.setLayoutManager(new LinearLayoutManager(this));
         getData();
-        
-
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-
-                Intent intent = new Intent(DashBoardActivity.this, LoginActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-
+        if(arrayList!=null){
+            if(arrayList.size()>0){
+                id_dashboard_recycler.setAdapter(new ConferenceAdapter(this,arrayList));
             }
-        });
-    }
+        }
 
-  
+   
 }
