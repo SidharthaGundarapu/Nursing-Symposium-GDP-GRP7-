87312ac1276665f7256af77ac9670c06077c4d25
AKV Phaneendra babu <indraakv@gmail.com>
2022-11-25 17:52:18 -0600

    Admin page code part1

---
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/AdminActivity.java
@@ -0,0 +1,24 @@
+package com.example.nursingsymposium.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.nursingsymposium.ConferenceDataModel;
+import com.example.nursingsymposium.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class AdminActivity extends AppCompatActivity {
+
+    FloatingActionButton addEvent;
+    RecyclerView id_dashboard_recycler;
+    ArrayList<ConferenceDataModel> models;
+
+   
+}
