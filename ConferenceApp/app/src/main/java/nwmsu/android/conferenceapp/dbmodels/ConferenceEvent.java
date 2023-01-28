d21e4a09670f1e7a5c583b06315d375f428c9d44
Dierale <89657232+Dierale@users.noreply.github.com>
2023-01-27 22:36:45 -0600

    Create ConferenceEvent.java
    Added Event model for conferences.  Need to add event_id attribute, figure out how to generate it, figure out how to generate conference_Id as well, then link them together when updating either.

---
+++ ConferenceApp/app/src/main/java/nwmsu/android/conferenceapp/dbmodels/ConferenceEvent.java
@@ -0,0 +1,89 @@
+package nwmsu.android.conferenceapp.dbmodels;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConferenceEvent {
+
+    protected String name;
+    protected String description;
+    protected Date start_date;
+    protected Date end_date;
+    protected boolean publicly_viewable_flag;
+    protected Date created_date;
+    protected Date updated_date;
+
+    protected String conference_id;
+    protected String[] usersAttending;
+
+    public ConferenceEvent() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public ConferenceEvent(String conference_id, String name, String description, Date start_date, Date end_date) {
+        this.name = name;
+        this.description = description;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.publicly_viewable_flag = false;
+        this.conference_id = conference_id;
+    }
+
+    public ConferenceEvent(String conference_id, String name, String description, Date start_date, Date end_date, boolean viewable) {
+        this.name = name;
+        this.description = description;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.publicly_viewable_flag = viewable;
+        this.conference_id = conference_id;
+    }
+
+    public Map<String, Object> getDocument() {
+        Map<String, Object> myModel = new HashMap<>();
+        myModel.put("name", this.name);
+        myModel.put("description", this.description);
+        myModel.put("start_date", this.start_date);
+        myModel.put( "end_date", this.end_date);
+        myModel.put("publicly_viewable_flag", this.publicly_viewable_flag);
+        myModel.put("conference_id", this.conference_id);
+        myModel.put("users_attending", this.usersAttending);
+        return myModel;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Date getStart_date() {
+        return this.start_date;
+    }
+
+    public Date getEnd_date() {
+        return this.end_date;
+    }
+
+    public boolean isPublicly_viewable_flag() {
+        return this.publicly_viewable_flag;
+    }
+
+    public Date getCreated_date() {
+        return this.created_date;
+    }
+
+    public Date getUpdated_date() {
+        return this.updated_date;
+    }
+
+    public String getConference_id() {
+        return this.conference_id;
+    }
+
+    public String[] getUsersAttending() {
+        return this.usersAttending;
+    }
+}
