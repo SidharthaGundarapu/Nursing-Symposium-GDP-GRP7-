56bf69481ce46a862585b036b50d00516da47d5f
Phani Padmanabha Naidu Ratnala <padmanabhanaiduratnala@gmail.com>
2023-02-03 22:10:58 -0600

    commit-2

--- NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/LoginActivity.java
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/LoginActivity.java
@@ -194,13 +194,9 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+   
     }
 
     private void showLoginFailed(@StringRes Integer errorString) {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
     }
-}
