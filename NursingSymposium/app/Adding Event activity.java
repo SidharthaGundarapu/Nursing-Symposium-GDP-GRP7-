4d8900bab4510eb861c542a3e18c16a605d52c28
SidharthaGundarapu <98143998+SidharthaGundarapu@users.noreply.github.com>
2023-02-17 19:47:59 -0600

    Commit -1

---
+++ NursingSymposium/app/Adding Event activity.java
@@ -0,0 +1,93 @@
+package com.example.nursingsymposium.admin;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.nursingsymposium.ConferenceAdapter;
+import com.example.nursingsymposium.ConferenceDataModel;
+import com.example.nursingsymposium.DashBoardActivity;
+import com.example.nursingsymposium.R;
+
+import java.util.ArrayList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
