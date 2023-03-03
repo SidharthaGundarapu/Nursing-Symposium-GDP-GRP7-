88356e9b6037c85a653bc99deddda7fe88006cdb
SidharthaGundarapu <98143998+SidharthaGundarapu@users.noreply.github.com>
2023-03-03 16:55:01 -0600

    Compact commit-2

--- NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/compactactivity.java
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/compactactivity.java
@@ -1,4 +1,4 @@
-package com.example.nursingsymposium;
+package main.java.nwmsu.android.nursingsymposium;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,3 +7,20 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+public class SpeakerActivity extends AppCompatActivity {
+
+    TextView speakerName,speakerabout;
+    ImageView eventpic;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speaker);
+        eventpic=findViewById(R.id.eventpic);
+        speakerabout=findViewById(R.id.speakerabout);
+        speakerName=findViewById(R.id.speakerName);
+        
+    
+
+}    
