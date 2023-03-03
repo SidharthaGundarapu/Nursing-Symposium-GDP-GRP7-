79c7fa9d8225f4c85b1ea4c377f59c1899dd1458
SidharthaGundarapu <98143998+SidharthaGundarapu@users.noreply.github.com>
2023-03-03 16:53:07 -0600

    compact commit-1

---
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/compactactivity.java
@@ -0,0 +1,9 @@
+package com.example.nursingsymposium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
