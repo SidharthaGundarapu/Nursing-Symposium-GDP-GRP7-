a18b0f80b5bea7d703ba55ed2d56db56a2ad51e6
Phani Padmanabha Naidu Ratnala <padmanabhanaiduratnala@gmail.com>
2023-03-03 22:27:51 -0600

    com1

--- NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/AdminActivity.java
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/AdminActivity.java
@@ -84,27 +84,7 @@ public class AdminActivity extends AppCompatActivity {
     }
     
 }
-package com.example.nursingsymposium.admin; 
- public class ConferenceModelAdd {
-    String key;
 
-    String imageKey;
-
-    String name;  
- 
- public String getImageKey() {
-        return imageKey;
-    }
-  public void setImageKey(String imageKey) {
-            this.imageKey = imageKey;
-        }
-         public String getName() {
-            return name;
-        }  
-       public void setName(String name) {
-            this.name = name;
-        } 
-        public String getKey() {
             return key;
         }
     public void setKey(String key) {
