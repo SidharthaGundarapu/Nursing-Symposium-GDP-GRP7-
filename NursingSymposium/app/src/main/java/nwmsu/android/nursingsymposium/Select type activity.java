c714d4e14afdde5c62381f895c9ef59e95ad2498
SidharthaGundarapu <98143998+SidharthaGundarapu@users.noreply.github.com>
2023-03-17 19:25:34 -0500

    second commit

--- NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/Select type activity.java
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/Select type activity.java
@@ -7,3 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+public class SelectTypeActivity extends AppCompatActivity {
+    
+    CardView student,admin;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_type);
+        student=findViewById(R.id.student);
+        admin=findViewById(R.id.admins);
