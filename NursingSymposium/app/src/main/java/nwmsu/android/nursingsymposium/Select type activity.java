b2445dbc3a11e5b876634c8b5bbf4da11e36776f
SidharthaGundarapu <98143998+SidharthaGundarapu@users.noreply.github.com>
2023-03-17 19:22:18 -0500

    first commit

---
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/Select type activity.java
@@ -0,0 +1,9 @@
+package com.example.nursingsymposium;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
