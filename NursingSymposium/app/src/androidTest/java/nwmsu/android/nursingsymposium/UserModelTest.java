fb5204df05d95f6d0836116f7e32c70024b9dcbe
Dierale <89657232+Dierale@users.noreply.github.com>
2023-03-24 22:46:32 -0500

    Update UserModelTest.java
    Added scaffolding for user unit tests & setup.

--- NursingSymposium/app/src/androidTest/java/nwmsu/android/nursingsymposium/UserModelTest.java
+++ NursingSymposium/app/src/androidTest/java/nwmsu/android/nursingsymposium/UserModelTest.java
@@ -1,9 +1,40 @@
 package nwmsu.android.nursingsymposium;
 
+import nwmsu.android.nursingsymposium.UserModel;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class UserModelTest {
 
+    @Mock
+    FirebaseAuthRepositoryType firebaseAuthRepositoryType;
+    @Mock
+    FirebaseUser firebaseUser;
+
+    @InjectMocks
+    UserModel userModel;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void createUser() {
+
+    }
+
+    @Test
+    public void updateUser() {
+        //        Mockito.when(firebaseAuthRepositoryType.getCurrentUser)
+    }
+
+    @Test
+    public void deleteUser() {
+
+    }
 }
