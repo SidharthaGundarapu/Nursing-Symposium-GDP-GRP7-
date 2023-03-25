7c4805e339d5d3b192b88d88691f56d1db6dd1df
Phani Padmanabha Naidu Ratnala <padmanabhanaiduratnala@gmail.com>
2023-03-24 23:09:32 -0500

    added selectiontypeactivity

---
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/selectiontypeactivity.java
@@ -0,0 +1,41 @@
+package com.nwmissouri.edu.conferencescheduler.adapter;
+
+import android.content.Intent;
+
+import android.view.LayoutInflater;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import androidx.recyclerview.widget.DiffUtil;
+
+import androidx.recyclerview.widget.ListAdapter;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.nwmissouri.edu.conferencescheduler.databinding.ListItemHeaderBinding;
+
+import com.nwmissouri.edu.conferencescheduler.databinding.ListItemSponsorBinding;
+
+import com.nwmissouri.edu.conferencescheduler.model.Sponsor;
+
+import com.nwmissouri.edu.conferencescheduler.utils.Constants;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import java.util.Collections;
+
+import java.util.LinkedHashSet;
+
+import java.util.List;
+
+import java.util.Map;
+
+import java.util.Set;
+
+import java.util.stream.Collectors;
+
+
