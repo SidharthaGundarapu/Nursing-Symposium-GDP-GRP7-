5a331a6ea567e5ab41c09d7ca8bce5a379a49b37
Dierale <89657232+Dierale@users.noreply.github.com>
2023-03-31 23:31:09 -0500

    Create RegistrationModelInteractor.java
    Added structure for RegistrationModelInteractor - class can be refactored to be displayed as UserModelInteractor. They should be the same thing ideally.

---
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/RegistrationModelInteractor.java
@@ -0,0 +1,22 @@
+package nwmsu.android.nursingsymposium;
+
+import nwmsu.android.nursingsymposium.data.model.RegistrationModel;
+
+public class RegistrationModelInteractor {
+
+    public static boolean createUser(String username, String password, String confirmPassword) {
+        return true;
+    }
+
+    public static boolean updateUser(int id, String username, String password, String confirmPassword) {
+        return true;
+    }
+
+    public static boolean deleteUser(int id) {
+        return true;
+    }
+
+    public static RegistrationModel getUserById(int id) {
+        return new RegistrationModel();
+    }
+}
