38115dd984a2a2b1a159954701d52cfd580ca3bf
Phani Padmanabha Naidu Ratnala <padmanabhanaiduratnala@gmail.com>
2023-04-14 19:28:18 -0500

    commit-3

--- NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/conferencefragmentation.java
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/conferencefragmentation.java
@@ -52,7 +52,26 @@ public class ConferenceFragment extends Fragment {
         });
 
     }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_conference, container, false);
+        id_dashboard_recycler=view.findViewById(R.id.id_dashboard_recycler);
+        id_dashboard_recycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+        arrayList=new ArrayList<>();
+        getData(arrayList);
+        conferenceAdapter= new ConferenceAdapter((DashBoardActivity) getActivity(),arrayList);
+        return view;
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        if(arrayList!=null){
+            if(arrayList.size()>=0){
+                id_dashboard_recycler.setAdapter(conferenceAdapter);
+            }
+        }
 
 
     }
