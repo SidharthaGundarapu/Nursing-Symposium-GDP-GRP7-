424e4ed7c896ae9c39d9fa7128dcf5cbe4d78d43
SidharthaGundarapu <98143998+SidharthaGundarapu@users.noreply.github.com>
2023-04-21 16:15:55 -0500

    aboutactivity-3

--- NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/AboutActivity.java
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/AboutActivity.java
@@ -10,3 +10,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.nursingsymposium.R;
+
+public class About  extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_about, container, false);
+
+    }
+}
