6e161daa68fbd0eae8c77839dc5a2ed20f5b5313
SidharthaGundarapu <98143998+SidharthaGundarapu@users.noreply.github.com>
2023-04-21 16:11:23 -0500

    aboutactivity-1

---
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/AboutActivity.java
@@ -0,0 +1,7 @@
+package com.example.nursingsymposium.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
