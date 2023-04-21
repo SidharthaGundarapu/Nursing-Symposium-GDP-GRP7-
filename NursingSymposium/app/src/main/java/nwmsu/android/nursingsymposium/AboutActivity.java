b7177650b7773e2d44f67ba4fa6b3431ab3cc10f
SidharthaGundarapu <98143998+SidharthaGundarapu@users.noreply.github.com>
2023-04-21 16:13:53 -0500

    aboutactivity-2

--- NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/AboutActivity.java
+++ NursingSymposium/app/src/main/java/nwmsu/android/nursingsymposium/AboutActivity.java
@@ -5,3 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.nursingsymposium.R;
