346225404f6daead7e0756d67b94712f94576511
AKV Phaneendra babu <98144010+IndraPhani@users.noreply.github.com>
2023-04-21 20:28:02 -0500

    Created the Splash activity

---
+++ ConferenceScheduler/app/src/main/java/com/nwmissouri/edu/conferencescheduler/model/SplashActivity.java
@@ -0,0 +1,58 @@
+package com.nwmissouri.edu.conferencescheduler.activities;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.nwmissouri.edu.conferencescheduler.databinding.ActivitySplashBinding;
+import com.nwmissouri.edu.conferencescheduler.utils.FirebaseUtilsManager;
+import com.nwmissouri.edu.conferencescheduler.utils.MySharedPreferences;
+import com.nwmissouri.edu.conferencescheduler.utils.ToastUtils;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    private ActivityResultLauncher<String[]> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), grantResults -> {
+                if (grantResults.containsValue(true)) {
+                    launchActivity();
+                } else {
+                    ToastUtils.showToast(this, "Permission Required");
+                }
+            });
+    private static final int SPLASH_TIME_OUT = 1000;
+
+    private MySharedPreferences pref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        com.nwmissouri.edu.conferencescheduler.databinding.ActivitySplashBinding binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        pref = MySharedPreferences.initialize(this);
+        new Handler().postDelayed(this::checkPermission, SPLASH_TIME_OUT);
+
+        FirebaseUtilsManager.initialize();
+
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+    }
+
+    
+
+}
